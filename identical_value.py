8c63e3a1bbf8ba894cb82e7215971f0f522ae255
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 20:06:31 +0800

    pseudocode

---
+++ identical_value.py
@@ -0,0 +1,12 @@
+# Exercise 5: Check if the first and last number of a list is the same
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# pseudocode
+
+#create a function to check if the list corresponds to the condition
+
+# print the random list
+
+# check if the first and last number is equal to each other
+
+# create a list and pirnt the result 
