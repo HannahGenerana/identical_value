9f7e368da01444565eaf0613c5928fecad9293cf
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 20:09:31 +0800

    created a condition to return true or false

--- identical_value.py
+++ identical_value.py
@@ -4,9 +4,16 @@
 # pseudocode
 
 #create a function to check if the list corresponds to the condition
+def checking_same_value (random_list):
 
 # print the random list
-
+    print (f"Given list {random_list}"
+           )
 # check if the first and last number is equal to each other
+    if random_list[0] == random_list[-1]:
+        return True
+    
+    else:
+        return False
 
-# create a list and pirnt the result 
+# create a list and print the result 
